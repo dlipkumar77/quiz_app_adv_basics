7f3c01c0a86a75be32a158245bfb143db0f69bba
deepak <dlipkumar77@gmail.com>
2025-01-10 12:08:17 +0530

    StatefulWidget & Split Code Quiz & Question Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import './start_screen.dart';
+import './quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        //title: Text('Quiz App'),
-        backgroundColor: const Color.fromARGB(255, 73, 5, 150),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              const Color.fromARGB(255, 78, 13, 151),
-              const Color.fromARGB(255, 107, 15, 168)
-            ],
-          ),
-        ),
-        child: StartScreen(),
-      ),
-    ),
-  ));
+  runApp(Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import './start_screen.dart';
+import './questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      //activeScreen = QuestionsScreen();
+      if (activeScreen is StartScreen) {
+        activeScreen = QuestionsScreen();
+      } else {
+        activeScreen = StartScreen();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          //title: Text('Quiz App'),
+          backgroundColor: const Color.fromARGB(255, 73, 5, 150),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color.fromARGB(255, 78, 13, 151),
+                const Color.fromARGB(255, 107, 15, 168)
+              ],
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
