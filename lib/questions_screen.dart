b5ba6a67aa66cb0feae9ac5b66a87a4255428031
deepak <dlipkumar77@gmail.com>
2025-01-10 18:05:02 +0530

    Dummy design column created

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -9,6 +9,31 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('What are the main building blocks of Flutter UIs?'),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Widgets'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Blocks'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Bricks'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Pieces'),
+          ),
+        ],
+      ),
+    );
   }
 }
